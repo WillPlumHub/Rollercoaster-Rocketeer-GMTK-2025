51927e139b858e43d4fb9662f59df9a257c4b41c
Miguel <kingofspadeslm567@gmail.com>
2025-07-30 16:49:35 -0700

    thruster particle effects

--- scripts/thrusters.gd
+++ scripts/thrusters.gd
@@ -1,15 +1,56 @@
+@tool
 extends Node2D
 class_name Thrusters
 
 const FORCE = 1000.0
 
+var body: RigidBody2D:
+	get: return get_parent()
+
 var fuel = 100.0
+var _wish_direction: Vector2
+var _thrusting: bool = false
+
+
+func _ready():
+	if !Engine.is_editor_hint():
+		if body is not RigidBody2D:
+			push_error("Parent is not a RigidBody2D!")
+
+
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+	
+	# Input
+	_thrusting = Input.is_action_pressed("launch.activate_thrust") && !is_zero_approx(fuel)
+	if _thrusting:
+		_wish_direction = (get_global_mouse_position() - global_position).normalized()
+
+	# Particle effects
+	for n: Node in get_children():
+		if n is CPUParticles2D:
+			n.global_rotation = 0.0
+			n.emitting = _thrusting
+			n.direction = -_wish_direction
 
 
 func _physics_process(delta: float) -> void:
-	var body = get_parent()
-	if Input.is_action_pressed("launch.activate_thrust") && !is_zero_approx(fuel):
-		var wish_dir = (get_global_mouse_position() - global_position).normalized()
-		if body is RigidBody2D && !wish_dir.is_zero_approx():
-			body.apply_central_force(FORCE * wish_dir)
+	if Engine.is_editor_hint():
+		return
+	
+	# Thrusting
+	if _thrusting:
+		if body is RigidBody2D && !_wish_direction.is_zero_approx():
+			body.apply_central_force(FORCE * _wish_direction)
 		fuel -= delta
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	if get_parent() is not RigidBody2D:
+		return["Parent must be a RigidBody2D!"]
+	return []
+	
+
+# I like the way you thrust, Cloud!
+#	- AoS
