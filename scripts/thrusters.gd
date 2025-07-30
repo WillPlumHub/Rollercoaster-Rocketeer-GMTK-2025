c622dcf9cd69671a9c2ff3e3e30ab6130992e1b7
Miguel <kingofspadeslm567@gmail.com>
2025-07-30 15:58:00 -0700

    mouse controlled thrusters

--- scripts/thrusters.gd
+++ scripts/thrusters.gd
@@ -3,9 +3,13 @@ class_name Thrusters
 
 const FORCE = 1000.0
 
+var fuel = 100.0
+
 
 func _physics_process(delta: float) -> void:
 	var body = get_parent()
-	var wish_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if body is RigidBody2D:
-		body.apply_central_force(FORCE * wish_dir)
+	if Input.is_action_pressed("launch.activate_thrust") && !is_zero_approx(fuel):
+		var wish_dir = (get_global_mouse_position() - global_position).normalized()
+		if body is RigidBody2D && !wish_dir.is_zero_approx():
+			body.apply_central_force(FORCE * wish_dir)
+		fuel -= delta
