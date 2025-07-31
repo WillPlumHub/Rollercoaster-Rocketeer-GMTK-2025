d51991887b48b8c53ccf3150cee41f9a2d4a537d
William Plummer <80426184+WillPlumHub@users.noreply.github.com>
2025-07-30 18:39:43 -0700

    l (#9)

--- Post Launch/Enemies/Obstacle.gd
+++ Post Launch/Enemies/Obstacle.gd
@@ -1,11 +1,33 @@
 extends Area2D
 
 var level_progress: LevelProgress
+var rotationSpeed = 1
+var ship: Node2D
+
 
 func _ready():
+	# Get LevelProgress (direct parent)
 	level_progress = get_parent() as LevelProgress
+	# Get Ship (sibling of LevelProgress, grandparent of this node)
+	var root = get_parent().get_parent()
+	ship = root.get_node("Ship")  # Make sure node name is exactly "Ship"
+	# Randomly determine rotation
+	var direction := 1
+	if randf() < 0.5:
+		direction = -1
+	var speed := deg_to_rad(90)  # 90 degrees per second
+	rotationSpeed = direction * speed
+
+
+func _process(delta):
+	rotation += rotationSpeed * delta
+
 
 func _on_area_entered(area: Area2D) -> void:
-		print("Asteroid hit by projectile!")
-		level_progress.speed -= 50
-		queue_free()
+	if (area.collision_layer & (1 << 0)):  # Layer 1 = player's body
+		if (ship.armor > 0):
+			ship.armor -= 1
+		else:
+			level_progress.speed -= 50
+			print("Speed: ", level_progress.speed)
+	queue_free()
--- Post Launch/Player/Projectile.gd
+++ Post Launch/Player/Projectile.gd
@@ -3,12 +3,20 @@ extends Area2D
 class_name Bullet
 
 var speed = 500  # pixels per second
+var brickThru = true
+
 
 func _ready():
 	add_to_group("projectile")
+
 
 func _process(delta):
 	position.y -= speed * delta
 	
 	if (global_position.y < -80):
+		queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if (area.collision_layer & (1 << 1) && brickThru): # Layer 2 = hazzards
 		queue_free()
