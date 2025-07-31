01aecfec874a6fcc2b239e4046aa96b0122584b8
Miguel <kingofspadeslm567@gmail.com>
2025-07-31 12:36:08 -0700

    HealthComponent takes appropriate damage
    HealthComponent now takes account of the normal of the collision

--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -35,12 +35,27 @@ func _ready() -> void:
 
 func _on_body_entered(n: Node2D):
 	if _parent is RigidBody2D && !_inv_timer && impact_damage:
-		var damage = (_parent.linear_velocity / 10.0).length()
-		print("%s took %s damage" % [_parent.name, damage])
-		if damage >= 10:
-			_inv_timer = get_tree().create_timer(1.0, false, true, false)
-			_inv_timer.timeout.connect(_on_timeout)
-			health -= damage
+		# Raycast to get the normal of contact
+		var space_state = _parent.get_world_2d().direct_space_state
+		var start_pos = _parent.global_position
+		var end_pos = _parent.global_position + (_parent.linear_velocity.normalized() * 1000.0)
+		var query = PhysicsRayQueryParameters2D.create( 
+			start_pos,
+			end_pos,
+			_parent.collision_mask, [_parent])
+		var result = space_state.intersect_ray(query)
+		#print(_parent.linear_velocity.normalized())
+		
+		if result:
+			var damage = (_parent.linear_velocity * result.normal).length() / 10.0
+			print("Contact from %s to %s" % [start_pos, result.position])
+			print("%s took %s damage" % [_parent.name, floor(damage)])
+			if damage >= 10:
+				_inv_timer = get_tree().create_timer(1.0, false, true, false)
+				_inv_timer.timeout.connect(_on_timeout)
+				health -= damage
+		#else:
+			#print("No contact from %s to %s" % [start_pos, end_pos])
 
 
 func _on_timeout():
