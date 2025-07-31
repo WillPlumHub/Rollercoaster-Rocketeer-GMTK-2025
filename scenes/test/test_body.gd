058eae8fbbe23af8bc9c1af6ceff86f006885786
Miguel <kingofspadeslm567@gmail.com>
2025-07-30 17:29:53 -0700

    rigid bodies take damage on impact with a health component attached

--- scenes/test/test_body.gd
+++ scenes/test/test_body.gd
@@ -1,33 +1,13 @@
-extends CharacterBody2D
-
-const SPEED = 10.0
-const JUMP_VELOCITY = -900.0
-
-@export var gravity_component: GravityComponent
+extends RigidBody2D
 
 
-func _physics_process(delta: float) -> void:
-	var acceleration: Vector2 = Vector2.ZERO
-	
-	# Add the gravity.
-	if not is_on_floor():
-		acceleration.y = gravity_component.gravity_force * delta
-	else:
-		acceleration.y = 0.0
+func _ready(): 
+	print($HealthComponent.health)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
-		acceleration.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		acceleration.x = direction * SPEED
-	else:
-		acceleration.x = 0.0
-		
-	velocity += acceleration.x * gravity_component.right + acceleration.y * gravity_component.up
-	rotation = lerp_angle(rotation, atan2(gravity_component.right.y, gravity_component.right.x), 0.1)
+func _on_health_component_killed() -> void:
+	queue_free()
 
-	move_and_slide()
+
+func _on_health_component_damaged() -> void:
+	print($HealthComponent.health)
--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -1,7 +1,8 @@
 extends Node
 class_name HealthComponent
 
-var max_health: int = 5
+@export var impact_damage: bool = true
+@export var max_health: int = 5
 @onready var health: int = max_health :
 	get:
 		return health
@@ -24,13 +25,22 @@ signal killed
 var _parent: Node2D:
 	get: return get_parent()
 
+var _inv_timer: SceneTreeTimer
+
 
 func _ready() -> void:
 	if _parent is RigidBody2D:
 		_parent.body_entered.connect(_on_body_entered)
-		_parent.body_exited.connect(_on_body_entered)
 
 
 func _on_body_entered(n: Node2D):
-	if _parent is RigidBody2D:
-		print("Owie! I got hit at a speed of %s" % _parent.linear_velocity)
+	if _parent is RigidBody2D && !_inv_timer:
+		var damage = (_parent.linear_velocity / 10.0).length()
+		if damage >= 50:
+			_inv_timer = get_tree().create_timer(1.0, false, true, false)
+			_inv_timer.timeout.connect(_on_timeout)
+			health -= damage
+
+
+func _on_timeout():
+	_inv_timer = null
