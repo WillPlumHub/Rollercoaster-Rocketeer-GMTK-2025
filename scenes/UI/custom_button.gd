353171ec6f254ac0315e8a6a73fb0f2c3a3dafbb
FluffyFishSlimy <87944269+FluffyFishSlimy@users.noreply.github.com>
2025-08-02 00:14:18 +0200

    Add custom UI buttons, new assets, and update menus
    Introduces a reusable custom_button scene and script with animated scaling and sound on press. Adds new planet and SFX assets, updates the main menu and credits scenes to use the new button, improves layout and fonts, and updates the SoundManager to support button press sounds. Also enhances menu background floating objects with random planet textures.

---
+++ scenes/UI/custom_button.gd
@@ -0,0 +1,16 @@
+extends Button
+
+func _ready() -> void:
+	pivot_offset.x = size.x/2
+	pivot_offset.y = size.y/2
+
+func _on_mouse_entered() -> void:
+	var t = create_tween().set_trans(Tween.TRANS_CIRC)
+	t.tween_property(self, "scale", Vector2(1.2, 1), 0.2)
+
+func _on_mouse_exited() -> void:
+	var t = create_tween().set_trans(Tween.TRANS_CIRC)
+	t.tween_property(self, "scale", Vector2(1, 1), 0.2)
+
+func _on_pressed() -> void:
+	SoundManager.play_sound('button_press', 1, 0)
