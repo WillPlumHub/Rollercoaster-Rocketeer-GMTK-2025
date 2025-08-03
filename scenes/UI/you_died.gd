6fefbc8365dd6954b786aa88ffbf699d029ac3bf
Miguel <kingofspadeslm567@gmail.com>
2025-08-03 02:09:14 -0700

    init work on sfx

--- scenes/UI/you_died.gd
+++ scenes/UI/you_died.gd
@@ -7,7 +7,7 @@ func _on_return_to_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/UI/menu.tscn")
 
 func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/UI/cutscenes/cutscene_1.tscn")
+	get_tree().reload_current_scene()
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
